41cc95121a452a2e6f99bb1d85e0143c7e9fdc0e
Alexander Zaikin <ay.zaikin@gmail.com>
2022-10-19 10:36:19 +0500

    Add leaft fractal in winforms

--- Exercise4_cs/Form1.Designer.cs
+++ Exercise4_cs/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.mainPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(738, 537);
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Location = new System.Drawing.Point(775, 12);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(100, 23);
+            this.aTextBox.TabIndex = 1;
+            this.aTextBox.Text = "0.7";
+            this.aTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.aTextBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(756, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(885, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aTextBox);
+            this.Controls.Add(this.mainPictureBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Exercise4_cs/Form1.cs
+++ Exercise4_cs/Form1.cs
@@ -1,21 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace Exercise4_cs
 {
     public partial class Form1 : Form
     {
+        private Bitmap image;
+        private float a = 0.7f, b = 0.3f, c = 0.5f, d = 0.3f, x0 = 1;
+        private int maxDepth = 20;
+        private float scale = 500;
+        private int xOffset = 300;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void aTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (float.TryParse(aTextBox.Text.Replace('.', ','), out a))
+                {
+                    RedrawImage();
+                }
+            }
+        }
+
+        private void LeafFractal(float x, float y, int depth = 0)
+        {
+            if (depth == maxDepth)
+            {
+                return;
+            }
+            PaintPixel(x, y);
+
+            LeafFractal(
+                a * x + b * y,
+                b * x - a * y,
+                depth + 1);
+
+            LeafFractal(
+                c * (x - x0) - d * y + x0,
+                d * (x - x0) + c * y,
+                depth + 1);
+        }
+
+        private void PaintPixel(float x, float y)
+        {
+            image.SetPixel(
+                (int)(x * scale + mainPictureBox.Width / 2 - xOffset),
+                (int)(-y * scale + mainPictureBox.Height / 2),
+                Color.Green);
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            RedrawImage();
+        }
+
+        private void RedrawImage()
+        {
+            if (image != null)
+            {
+                image.Dispose();
+            }
+            image = new Bitmap(mainPictureBox.Width, mainPictureBox.Height);
+            mainPictureBox.Image = image;
+            LeafFractal(1, 0);
+        }
     }
 }
